8714e40de49517019251574c150e3c4a47c5b31e
Jakub Mikulski <jmik12131@gmail.com>
2025-10-23 21:44:00 +0100

    Copy public ssh key with home manager

--- users/jakub/home.nix
+++ users/jakub/home.nix
@@ -5,6 +5,9 @@
     username = "jakub";
     homeDirectory = "/home/jakub";
 
+		# Copy public ssh key
+		file.".ssh/id_ed25519.pub".text = builtins.readFile ./public-keys/id_me.pub;
+
     # Backwards compatibility
     stateVersion = "24.11";
   };
