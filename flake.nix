15d22fc66e83cadd544790198ba216db119e8edd
Jakub Mikulski <jmik12131@gmail.com>
2026-05-03 20:06:12 +0100

    Add cachix binary cache

--- flake.nix
+++ flake.nix
@@ -8,6 +8,15 @@
     musnix.url = "github:musnix/musnix";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     {
       self,
--- modules/nixos/base/base.nix
+++ modules/nixos/base/base.nix
@@ -45,4 +45,18 @@
     	Defaults timestamp_timeout=300
     	Defaults timestamp_type=global
   '';
+
+  # Binary Cache Subsituters
+  nix = {
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };  
+
 }
