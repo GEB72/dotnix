4829721208e23cfe8485b8543cc970ff72c8a681
Jakub Mikulski <jmik12131@gmail.com>
2026-09-09 19:57:29 +0100

    Fix kanata autostart

--- modules/nixos/keyboard/keyboard.nix
+++ modules/nixos/keyboard/keyboard.nix
@@ -1,18 +1,18 @@
-{ config, self, ... }:
+{ self, ... }:
 {
   services.udev.extraRules =
     # Vial udev rules
     "KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", "
     + "ATTRS{serial}==\"*vial:f64c2b3c*\", MODE=\"0660\", "
-    + "GROUP=\"users\", TAG+=\"uaccess\", TAG+=\"udev-acl\" \n"
-    # Symlink keyboard to /dev/input/ udev rules (path changes on reboot)
-    + "SUBSYSTEMS==\"input\",  ATTRS{name}==\"ASUSTeK ROG OMNI RECEIVER Keyboard\", "
-    + "SYMLINK += \"keyboard\" ";
+    + "GROUP=\"users\", TAG+=\"uaccess\", TAG+=\"udev-acl\" ";
 
   # Enable Kanata
   services.kanata = {
     enable = true;
-    keyboards."kb".configFile = "${self}/modules/nixos/keyboard/kanata.kbd";
+    keyboards."kb" = {
+      configFile = "${self}/modules/nixos/keyboard/kanata.kbd";
+      extraArgs = [ "--wait-device-ms" "5000" ];
+    };
   };
 
 }
