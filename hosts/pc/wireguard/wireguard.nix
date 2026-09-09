1fef947e5b65d4e05702908e220bbf58b8713129
Jakub Mikulski <Jakub.Mikulski@realvnc.com>
2026-09-09 20:25:21 +0100

    Make host wireguard server

--- hosts/pc/wireguard/wireguard.nix
+++ hosts/pc/wireguard/wireguard.nix
@@ -1,6 +1,5 @@
 { config, ... }:
 {
-  # Load private key
   sops.secrets = {
     "pc/wireguard-key" = {
       group = config.users.users.systemd-network.group;
@@ -10,32 +9,41 @@
     };
   };
 
-  # Wireguard
   networking.wireguard = {
-    # Enable the service
-    enable = false;
+    enable = true;
 
-    # Set wg0 client interface
     interfaces = {
       wg0 = {
         privateKeyFile = config.sops.secrets."pc/wireguard-key".path;
 
-        ips = [ "10.0.0.2/24" ];
+        ips = [ "10.0.0.1/24" ];
         listenPort = 51820;
         mtu = 1420;
 
+        # IP forwarding for peers to reach Sunshine
+        postSetup = ''
+          ${config.networking.firewall.package}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o enp12s0 -j MASQUERADE
+          ${config.networking.firewall.package}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+          ${config.networking.firewall.package}/bin/iptables -A FORWARD -o wg0 -j ACCEPT
+        '';
+        postShutdown = ''
+          ${config.networking.firewall.package}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.0/24 -o enp12s0 -j MASQUERADE
+          ${config.networking.firewall.package}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+          ${config.networking.firewall.package}/bin/iptables -D FORWARD -o wg0 -j ACCEPT
+        '';
+
         peers = [
           {
-            publicKey = "UQ3113obKf6EZuKccgY5sIfD1HplVV0fOS8m5wT/OVQ=";
-            allowedIPs = [
-              "0.0.0.0/0"
-              "::/0"
-            ];
-            endpoint = "[86.178.57.18]:51820";
-            persistentKeepalive = 25;
+            # Steam Deck
+            # Replace with the actual public key from the Deck after running:
+            #   wg genkey | tee privatekey | wg pubkey > publickey
+            publicKey = "REPLACE_WITH_STEAMDECK_PUBLIC_KEY";
+            allowedIPs = [ "10.0.0.2/32" ];
           }
         ];
       };
     };
   };
+
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 }
--- modules/nixos/net/net.nix
+++ modules/nixos/net/net.nix
@@ -9,7 +9,7 @@
 
   # NAT
   networking.nat.enable = true;
-  networking.nat.externalInterface = "eth0";
+  networking.nat.externalInterface = "enp12s0";
   networking.nat.internalInterfaces = [ "wg0" ];
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
